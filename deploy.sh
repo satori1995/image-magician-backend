875ba799b9d9028f87bb8a8dd7d9b37cb1d66df9
小云同学 <shiinamashiro163@gmail.com>
2025-07-31 00:32:37 +0800

    httpx

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+# /bin/bash
+docker rm -f image-magician-backend
+docker rmi -f image-magician-backend
+docker build -f Dockerfile -t image-magician-backend . --no-cache
+docker run --name image-magician-backend -p 10001:80 -d image-magician-backend
+
+
--- lib/app.py
+++ lib/app.py
@@ -1,6 +1,6 @@
 import lib.app_core.middleware  # 注册中间件
 import lib.app_core.exception_handler  # 注册异常处理器
-import lib.handlers.images
+import lib.handlers
 from lib.app_core import app, global_context
 
 
